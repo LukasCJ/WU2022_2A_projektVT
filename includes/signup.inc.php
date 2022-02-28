e8d8a8bdcfb901994429fecbe42a9d2cb11fe860
LukasCJ <93259361+LukasCJ@users.noreply.github.com>
2022-02-28 12:01:00 +0100

    2022-02-28

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,1 +1,41 @@
-:)
+<?php
+
+if (isset($_POST["submit"])) { //check if signup method is correct
+    
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $username = $_POST["uid"];
+    $password = $_POST["pwd"];
+    $passwordRe = $_POST["pwdre"];
+
+    require_once 'dbh.inc.php';
+    require_once 'functions.inc.php';
+
+    if (emptyInputSignup($name, $email, $username, $password, $passwordRe) !== false) {
+        header("location: ../signup.php?error=emptinput")
+        exit();
+    }
+    if (invalidUid($username) !== false) {
+        header("location: ../signup.php?error=invaliduid")
+        exit();
+    }
+    if (invalidEmail($email) !== false) {
+        header("location: ../signup.php?error=invalidemail")
+        exit();
+    }
+    if (pwdMatch($password, $passwordRe) !== false) {
+        header("location: ../signup.php?error=differentpasswords")
+        exit();
+    }
+    if (uidExists($conn, $username) !== false) {
+        header("location: ../signup.php?error=usernametaken")
+        exit();
+    }
+
+    createUser($conn, $name, $email, $username, $password);
+
+}
+else {
+    header("location: ../signup.php") //send user back to signup.php
+    exit();
+}
--- signup.php
+++ signup.php
@@ -1,15 +1,17 @@
 <?php include_once 'header.php'; ?>
 
 <section id="signup_form"> <!--ska passa in i gridden!!-->
-    <h2>Sign up</h2>
-    <form action="signup.inc.php" method="post">
-        <input type="text" name="name" placeholder="Full Name...">
-        <input type="text" name="email" placeholder="Email..">
-        <input type="text" name="uid" placeholder="Username..">
-        <input type="password" name="pwd" placeholder="Password..">
-        <input type="password" name="pwdre" placeholder="Repeat Password...">
-        <button type="submit" name="submit">Sign Up</button>
-    </form>
+    <div class="section_header"><h1>Sign Up</h1></div>
+    <div class="section_main">
+        <form action="includes/signup.inc.php" method="post">
+            <input type="text" name="name" placeholder="Full Name...">
+            <input type="text" name="email" placeholder="Email..">
+            <input type="text" name="uid" placeholder="Username..">
+            <input type="password" name="pwd" placeholder="Password..">
+            <input type="password" name="pwdre" placeholder="Repeat Password...">
+            <button type="submit" name="submit">Sign Up</button>
+        </form>
+    </div>
 </section>
 
 <?php include_once 'footer.php' ?>
