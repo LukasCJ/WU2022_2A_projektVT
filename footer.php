add6736ef91cb9feaf71c86251c2981bf92e3b41
LukasCJ <93259361+LukasCJ@users.noreply.github.com>
2022-03-09 17:09:16 +0100

    2022-03-09_3
    Ger upp på lite grejer...

--- footer.php
+++
@@ -1,7 +0,0 @@
-        <footer class="grid_item-switch">
-            <div class="nav_item"><h2>Contacts</h2></div>
-        </footer>
-    </div>
-    <script src="js/script.js"></script>
-</body>
-</html>
--- js/script.js
+++ js/script.js
@@ -1,24 +1,13 @@
-const nav = document.getElementById('nav')
-const nav_op = document.getElementsById('nav_op')
-const nav_close = document.getElementsById('nav_close')
+const nav = document.getElementsByTagName('nav')[0];
+const nav_op = document.getElementById('nav_op');
+const nav_close = document.getElementById('nav_close');
 
-function toggleNav() {
-    nav.classList.toggle('nav-active')
+function openNav() {
+    nav.classList.add('nav-active');
+}
+function closeNav() {
+    nav.classList.remove('nav-active');
 }
 
-nav_op.addEventListener('click', toggleNav)
-
-
-const popular_scroll_right = document.getElementById('popular_scroll-right');
-const popular_scroll_left = document.getElementById('popular_scroll-left');
-const popular_list = document.getElementById('popular_list')
-
-function popularScrollL() {
-    popular_list.scrollLeft += 20;
-};
-function popularScrollR() {
-    popular_list.scrollRight -= 20;
-};
-
-popular_scroll_left.addEventListener('click', popularScrollL)
-popular_scroll_right.addEventListener('click', popularScrollR)
+nav_op.addEventListener('click', openNav);
+nav_close.addEventListener('click', closeNav);
