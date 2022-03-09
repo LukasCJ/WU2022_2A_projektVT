020046d06ed938b608857724431d9cb0a2231cc8
LukasCJ <lukas.johansson3@elev.ga.ntig.se>
2022-03-09 22:43:58 +0100

    2022-03-09_4
    Framsteg!

--- for_login_stuff/login.php
+++ for_login_stuff/login.php
@@ -1,14 +1,3 @@
 <?php include_once 'header.php'; ?>
 
-<section id="login_form"> <!--ska passa in i gridden!!-->
-    <div class="section_header"><h1>Log In</h1></div>
-    <div class="section_main">
-        <form action="includes/login.inc.php" method="post">
-            <input type="text" name="name" placeholder="USername/Email...">
-            <input type="password" name="pwd" placeholder="Password..">
-            <button type="submit" name="submit">Log In</button>
-        </form>
-    </div>
-</section>
-
 <?php include_once 'footer.php' ?>
--- js/script.js
+++ js/script.js
@@ -2,11 +2,16 @@ const nav = document.getElementsByTagName('nav')[0];
 const nav_op = document.getElementById('nav_op');
 const nav_close = document.getElementById('nav_close');
 
+const all = document.getElementsByTagName('body')[0];
+
 function openNav() {
     nav.classList.add('nav-active');
+    all.style['overflow-y'] = 'hidden';
 }
+
 function closeNav() {
     nav.classList.remove('nav-active');
+    all.style['overflow-y'] = 'scroll';
 }
 
 nav_op.addEventListener('click', openNav);
